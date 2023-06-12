2c27ab268d9bdc4416a76dd680c3594e31744c53
kanae213 <129595038+kanae213@users.noreply.github.com>
2023-06-12 18:43:48 +0900

    Add files via upload

--- kadai_007/calc.js
+++ kadai_007/calc.js
@@ -1,17 +1,17 @@
-const num =15;
+let num = 15;
 
 //3と５の倍数 
-if(( num % 3 && num % 5 ) ==0 ) {
+ if ( num % 3 === 0 && num % 5 ===0 ) {
   console.log('３と５の倍数です');
 }
 
 //3の倍数
-else if(( num % 3 ) == 0 ) {
+else if( num % 3  === 0 ) {
   console.log('3の倍数です');
 }
 
 //5の倍数
-else if(( num % 5 ) == 0 ) {
+else if( num % 5  === 0 ) {
 console.log('5の倍数です');
 }
 
